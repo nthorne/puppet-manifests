a0dc00456dbcb3e3922de52ecf0290b3ce9e49ae
nthorne <notrupertthorne@gmail.com>
2014-01-04 22:04:42 +0100

    Added shared dir

--- target-slaptopen/manifests/pkg/samba.pp
+++ target-slaptopen/manifests/pkg/samba.pp
@@ -1,12 +1,20 @@
 class target-slaptopen::pkg::samba {
   include common::pkg::samba
 
+  file {"${home}/shared":
+    ensure => "directory",
+    mode => 0644,
+    owner => $user,
+    group => $group,
+  }
+
   file { "/etc/samba/smb.conf":
-    require =>  Package["samba"],
+    require =>  [Package["samba"], File["${home}/shared"]],
     source  => "puppet:///modules/target-slaptopen/smb.conf",
   }
 
   file { "/etc/samba/smbusers":
+    before => File["/etc/samba/smb.conf"],
     require =>  Package["samba"],
     source  => "puppet:///modules/target-slaptopen/smbusers",
   }
