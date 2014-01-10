56c559b79775bd2b0e75f84f36ed2dcb8a424838
Niklas Thorne <notrupertthorne@gmail.com>
2014-01-10 21:55:00 +0100

    Added initscript for dropbox

--- target-slaptopen/manifests/pkg/dropbox.pp
+++ target-slaptopen/manifests/pkg/dropbox.pp
@@ -1,5 +1,22 @@
 class target-slaptopen::pkg::dropbox {
-  package {"dropbox":
-    ensure => installed,
+#  package {"dropbox":
+#    ensure => installed,
+#  }
+
+  file { "/etc/init.d/dropbox":
+#    require =>  Package["dropbox"],
+    mode => 0755,
+    owner => "root",
+    group => "root",
+    source  => "puppet:///modules/target-slaptopen/dropbox",
+  }
+
+  exec { "dropbox-rc-update":
+    require => File["/etc/init.d/dropbox"],
+    command => "update-rc.d dropbox defaults; /etc/init.d/dropbox start",
+    path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"],
+    user => "root",
+    group => "root",
+    logoutput => true,
   }
 }
