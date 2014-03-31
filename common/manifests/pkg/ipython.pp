5fd480b06fbabf93099420549dff47f0e46dd56a
Niklas Thorne <notrupertthorne@gmail.com>
2014-03-31 21:47:08 +0200

    Added ipython-notebook

--- common/manifests/pkg/ipython.pp
+++ common/manifests/pkg/ipython.pp
@@ -2,4 +2,9 @@ class common::pkg::ipython {
   package {"ipython":
     ensure => installed,
   }
+
+  package {"ipython-notebook":
+    require => Package["ipython"],
+    ensure => installed,
+  }
 }
