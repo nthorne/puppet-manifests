a38ca933afdbccdf7345432a5c15c7b1e86bf0da
Niklas Thorne <notrupertthorne@gmail.com>
2015-04-01 20:58:30 +0200

    Added virtualbox dependencies

--- target-slaptopen/manifests/pkg/virtualbox.pp
+++ target-slaptopen/manifests/pkg/virtualbox.pp
@@ -2,4 +2,12 @@ class target-slaptopen::pkg::virtualbox {
   package {"virtualbox":
     ensure => installed,
   }
+
+  package {"virtualbox-qt":
+    ensure => installed,
+  }
+
+  package {"virtualbox-dkms":
+    ensure => installed,
+  }
 }
