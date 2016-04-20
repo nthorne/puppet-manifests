d6f1c580a0456b3d575905c8d207f424b6f0655b
Niklas Thörne <notrupertthorne@gmail.com>
2016-04-20 21:51:54 +0200

    Corrected mksbp

--- target-slaptopen/manifests/scripts/mksbt.pp
+++ target-slaptopen/manifests/scripts/mksbt.pp
@@ -2,13 +2,10 @@ class target-slaptopen::scripts::mksbt {
   include target-slaptopen::pkg::scala
   include target-slaptopen::src::bin
 
-  exec {"install-mksbt.sh":
+
+  file {"${home}/bin/mksbt.sh":
     require => [File["${home}/bin"], Package["scala"]],
-    cwd => "${home}/bin",
-    creates => "${home}/bin/mksbt.sh",
-    user => $user,
     group => $group,
-    logoutput => true,
     mode => 0744,
     source => "puppet:///modules/target-slaptopen/mksbt.sh",
   }
