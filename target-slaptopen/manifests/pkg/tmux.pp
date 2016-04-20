703af7ea5a0ec68f46ad50caf23aee3f48a2a092
Niklas Thörne <notrupertthorne@gmail.com>
2016-04-20 21:52:37 +0200

    Switched to tmux-2.0 and tpm

--- target-slaptopen/manifests/pkg/tmux.pp
+++ target-slaptopen/manifests/pkg/tmux.pp
@@ -1,23 +1,48 @@
 class target-slaptopen::pkg::tmux {
   include target-slaptopen::pkg::git
-  include common::pkg::tmux
   include common::pkg::xsel
   include target-slaptopen::src::repos
 
+  file {"${home}/.tmux/plugins":
+    ensure => "directory",
+    mode => 0644,
+    owner => $user,
+    group => $group,
+  }
+
   file {"${home}/.tmux.conf":
-    require => Exec["clone-tmux-environment"],
+    require => [Exec["clone-tmux-environment"], Exec["clone-tmux-tpm"]],
     ensure => link,
     force => true,
     target => "${home}/repos/nthorne-tmux-environment/.tmux.conf",
     owner => $user,
     group => $group,
   }
+  exec {"tmux-dev":
+    creates => "/usr/bin/tmux",
+    command => "apt-get install -y python-software-properties software-properties-common && add-apt-repository -y ppa:pi-rho/dev && apt-get update && apt-get install -y tmux=2.0-1~ppa1~t",
+    path => ["/bin", "/usr/bin", "/usr/sbin"],
+    user => $user,
+    group => $group,
+    logoutput => true,
+  }
 
   exec {"clone-tmux-environment":
-    require => [Package["git"], File["${home}/repos"], Package["tmux"], Package["xsel"]],
+    require => [Package["xsel"], Package["git"], File["${home}/repos"], Exec["tmux-dev"]],
     cwd => "${home}/repos",
     creates => "${home}/repos/nthorne-tmux-environment",
     command => "git clone --recursive http://github.com/nthorne/nthorne-tmux-environment.git",
+    path => ["/bin", "/usr/bin", "/usr/sbin"],
+    user => $user,
+    group => $group,
+    logoutput => true,
+  }
+
+  exec {"clone-tmux-tpm":
+    require => [Package["git"], File["${home}/repos"], Exec["clone-tmux-environment"], File["${home}/.tmux/plugins"]],
+    cwd => "${home}/.tmux/plugins",
+    creates => "${home}/.tmux/plugins/tpm",
+    command => "git clone --recursive git clone https://github.com/tmux-plugins/tpm",
     path => ["/bin", "/usr/bin", "/usr/sbin"],
     user => $user,
     group => $group,
